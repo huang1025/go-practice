d7efadd24cd8a735b3ea1641e5548375410c4de4
huang <huang_jiangling@163.com>
2019-11-04 16:34:28 +0800

    goto,breae,continue;

--- partion1/05-for/main.go
+++ partion1/05-for/main.go
@@ -37,4 +37,38 @@ func main() {
 	for index, value := range name {
 		fmt.Println(index, value)
 	}
+
+	//goto
+	for i = 0; i < 10; i++ {
+		if i > 0 {
+			goto tag
+		}
+		for j = 0; j < 10; j++ {
+			fmt.Println(i, j)
+		}
+	}
+tag:
+	fmt.Println("结束循环")
+
+	//breake：跳出循环
+tag2:
+	for i = 0; i < 10; i++ {
+		if i > 0 {
+			break tag2
+		}
+		for j = 0; j < 10; j++ {
+			fmt.Println(i, j)
+		}
+	}
+
+	//continue：继续下次循环
+tag3:
+	for i = 0; i < 10; i++ {
+		if i > 0 {
+			continue tag3
+		}
+		for j = 0; j < 10; j++ {
+			fmt.Println(i, j)
+		}
+	}
 }
